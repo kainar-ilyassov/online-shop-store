ae94b1aff3de3e69df30f08207424df5c84761ad
kainar <kainar.ilyassov@gmail.com>
2024-02-27 01:36:36 +0600

    add navbar with categories and shopping cart

---
+++ components/ui/test.js
@@ -0,0 +1,11 @@
+const recordValues = [" ", "    "];
+
+recordValues.forEach((item) => {
+  switch (item) {
+    case " ":
+      return "This is a value with onle space";
+      break;
+    default:
+      break;
+  }
+});
