ccb1b4cd606c5484ccb760685a0db902f627fdf9
kainar <kainar.ilyassov@gmail.com>
2024-02-28 01:15:35 +0600

    add filters by color and size to product

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -14,6 +14,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
           className
         )}
         ref={ref}
+        {...props}
       >
         {children}
       </button>
